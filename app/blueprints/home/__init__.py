d1c446a869bb190ceb5bc191d3abd8eb3aa6cb43
Igor Gonçalves <igorgoncalves@localhost.localdomain>
2018-12-28 03:12:36 -0300

    Mudança pra snake_case e remoção de pasta migrations

--- app/blueprints/home/__init__.py
+++ app/blueprints/home/__init__.py
@@ -3,12 +3,12 @@ from app.domain.service.IndicadorService import IndicadorService
 
 home = Blueprint('home', __name__)
 
-_indicadorService = IndicadorService()
+_service_indicador = IndicadorService()
 
 @home.route("/")
-def hello():
-    # indicadorService.create("Produção de frutas", "medir produção de frutas", 1)
-    indicadores = _indicadorService.get_all()
+def hello():    
+    indicadores = _service_indicador.get_all()
+    
     for indicador in indicadores:
         print (str(indicador))
 
--- tests/test_indicador.py
+++ tests/test_indicador.py
@@ -2,23 +2,23 @@ from app.domain.service.IndicadorService import IndicadorService
 from app.domain.repository.IndicadorRepository import IndicadorRepository
 from app.models.Indicador import Indicador
 
-repositoryIndicador =  IndicadorRepository()
-serviceIndicador =  IndicadorService()
+repository_indicador =  IndicadorRepository()
+service_indicador =  IndicadorService()
 
 # Testes de repositorio
 def test_r_get_by_id():
-    assert  isinstance(repositoryIndicador.get_by_id(1), Indicador)
+    assert  isinstance(repository_indicador.get_by_id(1), Indicador)
 
 def test_s_create():    
-    repositoryIndicador.create(Indicador(nome="test", objetivo="test", periodicidade=0))
-    ultimo = repositoryIndicador.get_all()[-1]    
+    repository_indicador.create(Indicador(nome="test", objetivo="test", periodicidade=0))
+    ultimo = repository_indicador.get_all()[-1]    
     assert  ultimo.nome == "test"
 
 def test_s_delete():        
-    ultimo = repositoryIndicador.get_all()[-1]    
-    repositoryIndicador.delete(ultimo)
-    assert  repositoryIndicador.get_by_id(ultimo.id) == None
+    ultimo = repository_indicador.get_all()[-1]    
+    repository_indicador.delete(ultimo)
+    assert  repository_indicador.get_by_id(ultimo.id) == None
 
 # Testes de serviço
 def test_s_get_by_id():
-    assert  isinstance(serviceIndicador.get_by_id(1), Indicador)
+    assert  isinstance(service_indicador.get_by_id(1), Indicador)
