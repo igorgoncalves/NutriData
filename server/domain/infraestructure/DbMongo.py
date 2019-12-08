2958297a7746bdfbb600f2f9cb9c5e877674bcb8
Igor Gonçalves <igor_goncalves@live.com>
2019-12-08 15:42:42 -0300

    fix: conexão com mongo

--- server/domain/infraestructure/DbMongo.py
+++ server/domain/infraestructure/DbMongo.py
@@ -1,3 +1,3 @@
 from mongoengine import connect
 
-connect('dbNutridata', host='172.21.0.2', port=27017, username='root', password='example', authentication_source='admin')
+connect('dbNutridata', host='mongo', port=27017, username='root', password='example', authentication_source='admin')
