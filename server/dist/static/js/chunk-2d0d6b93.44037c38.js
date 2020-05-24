a0df62877f5ff5d6df6dc1f157ca4ffd924752aa
Igor Gonçalves <igor_goncalves@live.com>
2020-05-23 22:52:16 -0300

    :rocket: front build

---
+++ server/dist/static/js/chunk-2d0d6b93.44037c38.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0d6b93"],{7488:function(n,u,l){"use strict";l.r(u);var c,e,o=l("2877"),p={},s=Object(o["a"])(p,c,e,!1,null,null,null);u["default"]=s.exports}}]);
+//# sourceMappingURL=chunk-2d0d6b93.44037c38.js.map
--- server/dist/static/js/chunk-2d0d6b93.494ab3d5.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0d6b93"],{7488:function(n,u,l){"use strict";l.r(u);var c,e,o=l("2877"),p={},s=Object(o["a"])(p,c,e,!1,null,null,null);u["default"]=s.exports}}]);
-//# sourceMappingURL=chunk-2d0d6b93.494ab3d5.js.map
